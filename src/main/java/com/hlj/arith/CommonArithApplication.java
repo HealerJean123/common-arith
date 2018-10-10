030706f4df2766efa5066881f77e8194adb7a7d3
HealerJean123 <mxzdhealer@gmail.com>
2018-10-10 13:33:24 +0800

    common-arith

---
+++ src/main/java/com/hlj/arith/CommonArithApplication.java
@@ -0,0 +1,12 @@
+package com.hlj.arith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonArithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonArithApplication.class, args);
+    }
+}
