f66a07ff977c4c72bdcbfc176ee8bc533a5a2d1d
zhangyujin <zhangyujin@xiaomi.com>
2020-02-24 00:23:36 +0800

    最长回文解析

--- src/main/java/com/hlj/arith/demo00022_最长回文子串/最长回文子串.java
+++ src/main/java/com/hlj/arith/demo00022_最长回文子串/最长回文子串.java
@@ -1,5 +1,7 @@
 package com.hlj.arith.demo00022_最长回文子串;
 
+import org.junit.Test;
+
 /**
 作者：
 
@@ -10,22 +12,27 @@ package com.hlj.arith.demo00022_最长回文子串;
 */
 public class 最长回文子串 {
 
+    @Test
+    public void test(){
+        System.out.println(longestPalindrome("banana"));
+    }
+
+
     public String longestPalindrome(String s) {
         int len = s.length();
+        //只有一个字符的时候，肯定是回文
         if (len < 2) {
             return s;
         }
 
+        // 初始化数组dp，全部设置为true
         boolean[][] dp = new boolean[len][len];
-
-        // 初始化
         for (int i = 0; i < len; i++) {
             dp[i][i] = true;
         }
 
         int maxLen = 1;
         int start = 0;
-
         for (int j = 1; j < len; j++) {
             for (int i = 0; i < j; i++) {
 
