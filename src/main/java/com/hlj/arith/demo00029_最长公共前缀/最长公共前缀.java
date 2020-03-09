0048e8655078dd25ac80d0a220c46f2741101f16
HealerJean <healerjean@gmail.com>
2020-03-09 15:39:48 +0800

    三数之和

--- src/main/java/com/hlj/arith/demo00029_最长公共前缀/最长公共前缀.java
+++ src/main/java/com/hlj/arith/demo00029_最长公共前缀/最长公共前缀.java
@@ -26,34 +26,34 @@ import org.junit.Test;
 public class 最长公共前缀 {
 
     @Test
-    public void test(){
-        String[] strs = new String[]{"flower","flow","flight"} ;
+    public void test() {
+        String[] strs = new String[]{"flower", "flow", "flight"};
         System.out.println(longestCommonPrefix(strs));
 
     }
 
     public String longestCommonPrefix(String[] strs) {
-        int strsLength = strs.length ;
-     if (strsLength == 0 ){
-         return "" ;
-     }
+        int strsLength = strs.length;
+        if (strsLength == 0) {
+            return "";
+        }
 
-     //初始化第一个要比较的字符串
-     String str = strs[0] ;
-     //从第二个开始和第一个字符串从0位置开始对比
-     for (int i = 1 ; i< strsLength ; i++){
-         //开始比对字符串
-         int j = 0 ;
-         for (; j < str.length() && j < strs[i].length() ; j++ ){
-             //如果不相等的则退出，记得此时j是没有匹配的
-            if (str.charAt(j) != strs[i].charAt(j)){
-                break;
+        //初始化第一个要比较的字符串
+        String str = strs[0];
+        //从第二个开始和第一个字符串从0位置开始对比
+        for (int i = 1; i < strsLength; i++) {
+            //开始比对字符串
+            int j = 0;
+            for (; j < str.length() && j < strs[i].length(); j++) {
+                //如果不相等的则退出，记得此时j是没有匹配的
+                if (str.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
             }
-         }
-         //执行到这里说明有匹配的，则从0开始截取，（因为上面break了，所以不会包含j的）
-         str = str.substring(0,j ) ;
-     }
-     return str ;
+            //执行到这里说明有匹配的，则从0开始截取，（因为上面break了，所以不会包含j的）
+            str = str.substring(0, j);
+        }
+        return str;
     }
 
 }
