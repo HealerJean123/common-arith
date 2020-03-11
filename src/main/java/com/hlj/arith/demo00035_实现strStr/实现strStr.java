b413a38032ec1797cfea614b63e0d32906a70523
HealerJean <healerjean@gmail.com>
2020-03-11 21:05:21 +0800

    kmp字符串匹配算法

--- src/main/java/com/hlj/arith/demo00035_实现strStr/实现strStr.java
+++ src/main/java/com/hlj/arith/demo00035_实现strStr/实现strStr.java
@@ -2,6 +2,8 @@ package com.hlj.arith.demo00035_实现strStr;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
 作者：HealerJean
 题目：实现 strStr()
@@ -19,33 +21,71 @@ public class 实现strStr {
 
     @Test
     public void test(){
-        System.out.println("".length());
-        // String haystack = "hello", needle = "ll" ;
-        // String haystack = "a", needle = "a" ;
-        String haystack = "mississippi", needle = "issip" ;
-
-
-        System.out.println(strStr(haystack, needle));
+        System.out.println(Arrays.toString(getNext("ABCDABDC")));
+        System.out.println(strStr("ABCABCDABDC", "ABCDABDC"));
     }
 
-    public int strStr(String haystack, String needle) {
-        //空字符串肯定匹配，且为0
-        if (needle.length() == 0 ){
+
+    public int strStr(String txt, String pattern) {
+        if (pattern.length() == 0) {
             return 0;
         }
+        //i 表示 text 中的位置，j 表示 find 中的位置
+        int[] next = getNext(pattern);
+        //遍历 txt 中的字符
+        for (int i = 0, j = 0; i < txt.length(); i++) {
+            //while放在开头，因为是刚刚开始匹配，如果不成立，next数组马上回溯
+            // j!= 0 但是不相等，表示刚刚经过匹配，这里是 KMP 算法的关键点，移动位置为回溯 next[j]
+            while (j != 0 && txt.charAt(i) != pattern.charAt(j)) {
+                j = next[j - 1];
+            }
+            //如果 i 位置和 j 位置的字符相同，待匹配字符串移动一位
+            if (txt.charAt(i) == pattern.charAt(j)) {
+                j++;
+            }
 
-        int i = 0 ;
-        for (int j = 0 ; j < haystack.length() ; j ++){
-            if (needle.charAt(i) == haystack.charAt(j)){
-                if (i == needle.length()-1){
-                    return j-needle.length()+1;
-                }
-                i++;
-            }else {
-                i = 0 ;
+            //在上面的if中 j++ 会比指针大1，当j等于待匹配长度的时候，表示到结尾了
+            if (j == pattern.length()) {
+                // i当前匹配到的地方，最后求的是txt字符串刚开始匹配的位置，所以 i - j + 1
+                return i - j + 1;
             }
         }
-        return  -1 ;
+        return -1;
     }
+
+    /**
+     * 获取next数组
+     */
+    public static int[] getNext(String sub) {
+        int[] next = new int[sub.length()];
+        //第一个和第二个是0，因为同时不存在前缀和后缀
+        next[0] = 0;
+        next[1] = 0;
+
+        // 因为 next[0]  next[1] 已经确定了，要开始找 next[2]，所以初始化 i = 1
+        // k 为 next[i] 当前的值，初始化的时候，next[1] = 0 ,所以k为0
+        int i = 1, k = 0;
+        // 所以i next[i+1] 是通过 next[i] 求的，i 不会超过 sub.length() - 1
+        while (i < sub.length() - 1) {
+            //如果字符串位置 `i `和位置 `next[i] `处的两个字符相同，则 `next[i+1]` = `next[i] + 1`
+            if (sub.charAt(i) == sub.charAt(k)) {
+                next[i + 1] = k + 1;
+                //因为上面 k 为next[i]，while下一步执行的就是i + 1
+                //上面 next[i + 1] 已经给出值了，所以继续执行while的话， i 和 k 都要加 1（i指针移动加1，k为值加1）
+                i++;
+                k++;
+            } else if (k == 0) {
+                //k = 0 并且没有匹配，当然为0喽，k也不需要+1了
+                next[i + 1] = 0;
+                i++;
+            } else {
+                // 往前好回溯，这个时候k是大于0的，但是上面第一个比较的时候，没有成功。
+                // 为了再类似于暴力法那样重新开始匹配，按照我们找出的规律往前回溯，代表性例子就知道了
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
 
 }
