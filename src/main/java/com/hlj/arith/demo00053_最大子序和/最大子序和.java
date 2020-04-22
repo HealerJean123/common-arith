58ad1029138fc48990e150f6936b2e35d0fa105f
HealerJean <healerjean@gmail.com>
2020-04-22 18:50:05 +0800

    最大子序和

---
+++ src/main/java/com/hlj/arith/demo00053_最大子序和/最大子序和.java
@@ -0,0 +1,31 @@
+package com.hlj.arith.demo00053_最大子序和;
+
+import org.junit.Test;
+
+/**
+ * @author HealerJean
+ * @ClassName 最大子序和
+ * @date 2020/4/22  18:25.
+ * @Description
+ */
+public class 最大子序和 {
+
+    @Test
+    public void test(){
+
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+    public int maxSubArray(int[] nums) {
+        int maxTemp = nums[0];
+        int max = nums[0];
+        for(int i = 1; i < nums.length; ++i) {
+            //当前元素, 当前元素位置的最大和
+            maxTemp = Math.max(nums[i], maxTemp + nums[i]);
+
+            max = Math.max(max, maxTemp);
+        }
+        return max;
+    }
+
+}
