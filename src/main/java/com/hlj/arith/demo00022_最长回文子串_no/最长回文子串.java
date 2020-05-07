ed020aa46d476735d1af68fd491f79daa5b74847
HealerJean <healerjean@gmail.com>
2020-05-07 16:43:46 +0800

    最大矩形

--- src/main/java/com/hlj/arith/demo00022_最长回文子串_no/最长回文子串.java
+++ src/main/java/com/hlj/arith/demo00022_最长回文子串_no/最长回文子串.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 /**
 作者：
 题目：最长回文子串
-解题思路：
-
+ 回文串：是指这个字符串无论从左读还是从右读，所读的顺序是一样的
+ 给定一个字符串 `s`，找到 `s` 中最长的回文子串。你可以假设 `s` 的最大长度为 1000。
+解题思路：动态规划
 */
 public class 最长回文子串 {
 
@@ -18,12 +19,12 @@ public class 最长回文子串 {
 
     public String longestPalindrome(String s) {
         int len = s.length();
-        //只有一个字符的时候，肯定是回文
+        //只有一个字符的时候，或者是空字符串
         if (len < 2) {
             return s;
         }
 
-        // 初始化数组dp，全部设置为true
+        // 初始化数组dp （i到j为回文），将对称点设置为true 也就是只有一个字符的情况
         boolean[][] dp = new boolean[len][len];
         for (int i = 0; i < len; i++) {
             dp[i][i] = true;
@@ -32,17 +33,20 @@ public class 最长回文子串 {
         int maxLen = 1;
         int start = 0;
         for (int j = 1; j < len; j++) {
-            for (int i = 0; i < j; i++) {
+            for (int i = j-1; i >= 0; i--) {
 
                 if (s.charAt(i) == s.charAt(j)) {
+                    //如果字符串是 ab 或者 a ，aba ，则肯定是回文，否则如果i和j所在字符串相等的话
                     if (j - i < 3) {
                         dp[i][j] = true;
                     } else {
                         dp[i][j] = dp[i + 1][j - 1];
                     }
+                  //如果i和j的字符不相等，则肯定是false
                 } else {
                     dp[i][j] = false;
                 }
+
 
                 // 只要 dp[i][j] == true 成立，就表示子串 s[i, j] 是回文，此时记录回文长度和起始位置
                 if (dp[i][j]) {
@@ -54,6 +58,7 @@ public class 最长回文子串 {
                 }
             }
         }
+        //包头不包尾
         return s.substring(start, start + maxLen);
     }
 
--- src/main/java/com/hlj/arith/demo00063_最大矩形/最大矩形.java
+++ src/main/java/com/hlj/arith/demo00063_最大矩形/最大矩形.java
@@ -2,6 +2,9 @@ package com.hlj.arith.demo00063_最大矩形;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
 作者：HealerJean
 题目：给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
@@ -15,6 +18,17 @@ import org.junit.Test;
          ]
          输出: 6
  解题思路：
+
+ {'1', '0', '1', '0', '0'},
+ {'1', '0', '1', '1', '1'},
+ {'1', '1', '1', '1', '1'},
+ {'1', '0', '0', '1', '0'}
+
+ 变成如下的，求矩形
+ {1, 0, 1, 0, 0},
+ {1, 0, 3, 2, 1},
+ {5, 4, 3, 2, 1},
+ {1, 0, 0, 1, 0}
 */
 public class 最大矩形 {
 
@@ -29,36 +43,48 @@ public class 最大矩形 {
 
         System.out.println(maximalRectangle(matrix));
     }
-
     public int maximalRectangle(char[][] matrix) {
+        //边界条件
+        if(null == matrix || 0 == matrix.length) {
+            return 0;
+        }
 
-        int max = 0 ;
-        for (int i = 0 ; i < matrix.length ; i++){
-            for (int j = 0 ; j < matrix[0].length ; j++){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+
+        //从倒数第一列开始 dp[i][n-1]
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][n-1] ==  '1'){
+                dp[i][n - 1] = 1 ;
+            }
+        }
+
+        //从倒数第二列开始继续往前推，数组默认就是0，所以只要求出1的就可以了
+        for (int i = 0; i < m; i++) {
+            for (int j = n - 2; j >= 0; j--) {
                 if (matrix[i][j] == '1'){
-                    max = Math.max(xunzhao(matrix, i, j), max);
+                    dp[i][j] =  1 + dp[i][j + 1];
                 }
             }
         }
 
-        return max ;
-    }
+        int res = 0;
 
-    public int xunzhao(char[][] matrix, int i, int j) {
-        if (i >= 0 && i < matrix.length
-            && j >= 0 && j < matrix[0].length
-            && matrix[i][j] == '1') {
-
-            matrix[i][j] = '0';
-            int num = xunzhao(matrix, i - 1, j)
-                    + xunzhao(matrix, i + 1, j)
-                    + xunzhao(matrix, i, j - 1)
-                    + xunzhao(matrix, i, j + 1)
-                    + 1;
-            matrix[i][j] = '2';
-            return num;
+        //以列推进。计算柱状最大面积,将 行坐标i放到栈里面去
+        for(int j = 0; j < n; j++) {
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+            for(int i = 0; i < m; i++) {
+                while (stack.peek() != -1 && dp[i][j] < dp[stack.peek()][j]) {
+                    res = Math.max(res, dp[stack.pop()][j] * (i - stack.peek() -1));
+                }
+                stack.push(i);
+            }
+            while (stack.peek() != -1){
+                res = Math.max(res,  dp[stack.pop()][j] * (m - stack.peek() -1));
+            }
         }
-        return 0 ;
+        return res;
     }
-
 }
