a544647c6843b3d40fb60bb7d2528d13ea047e64
HealerJean <healerjean@gmail.com>
2020-07-27 10:36:12 +0800

    判断自序列

--- src/main/java/com/hlj/arith/demo00118_判断子序列/判断子序列.java
+++ src/main/java/com/hlj/arith/demo00118_判断子序列/判断子序列.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 public class 判断子序列 {
 
     @Test
-    public void test(){
+    public void test() {
         // System.out.println(isSubsequence("abc", "ahbgdc"));
         System.out.println(isSubsequence("axc", "ahbgdc"));
 
@@ -28,10 +28,10 @@ public class 判断子序列 {
 
     public boolean isSubsequence(String s, String t) {
 
-        int i = 0 ;
-        int j = 0 ;
-        while (i < s.length() &&  j < t.length()){
-            if (t.charAt(j) == s.charAt(i)){
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (t.charAt(j) == s.charAt(i)) {
                 i++;
             }
             j++;
