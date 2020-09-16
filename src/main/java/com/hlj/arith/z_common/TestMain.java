3d7bf42e870af5bff4670846619081c7b1cd0b25
HealerJean <healerjean@gmail.com>
2020-09-16 10:39:40 +0800

    翻转二叉树

--- src/main/java/com/hlj/arith/z_common/TestMain.java
+++ src/main/java/com/hlj/arith/z_common/TestMain.java
@@ -16,22 +16,32 @@ import java.util.*;
 @Slf4j
 public class TestMain {
 
-
-
     @Test
     public void test() {
         int[] candidates = {2, 3, 6, 7};
         int target = 7;
         System.out.println(combinationSum(candidates, target));
     }
-
+    /**
+    * @description
+    * @params
+    * @return
+    * @author  HealerJean
+    * @date    2020/9/15 16:44
+    */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
         method(0, candidates, target, res, linkedList);
         return res;
     }
-
+    /**
+    * @description
+    * @params
+    * @return
+    * @author  HealerJean
+    * @date    2020/9/15 16:49
+    */
     public void method(int index, int[] candidates, int target, List<List<Integer>> res,LinkedList<Integer> linkedList ){
         if (target == 0){
             res.add(new ArrayList<>(linkedList));
