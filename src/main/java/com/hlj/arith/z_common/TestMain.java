cf49d93f7b7469b3f07d80c62547e95f695597d7
HealerJean <healerjean@gmail.com>
2020-10-14 14:14:56 +0800

    环形链表

--- src/main/java/com/hlj/arith/z_common/TestMain.java
+++ src/main/java/com/hlj/arith/z_common/TestMain.java
@@ -1,6 +1,5 @@
 package com.hlj.arith.z_common;
 
-import com.hlj.arith.z_common.treeNode.TreeNodeResources;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -18,58 +17,34 @@ import java.util.*;
 public class TestMain {
 
     @Test
-    public void test() {
-        System.out.println(generateParenthesis(3));
-
+    public void test(){
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+        System.out.println( Arrays.toString(nums));
     }
 
-    public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-         dsf(n, n , "",  res);
-         return res;
-    }
 
-    public void dsf(int left , int right, String str,List<String> res){
-        if (left ==0 && right == 0){
-            res.add(str);
-        }
-        if (left > right){
-            return ;
-        }
-
-        if (left > 0){
-            dsf(left-1,right , str+"(",  res);
-        }
-        if(right > 0){
-            dsf(left, right-1, str+")", res);
-        }
-
-    }
-
-    public TreeNode initTreeNode() {
-        TreeNode treeNode1 = new TreeNode(3, null, null);
-        TreeNode treeNode2 = new TreeNode(6, null, null);
-        TreeNode treeNode3 = new TreeNode(4, treeNode1, treeNode2);
-        TreeNode treeNode4 = new TreeNode(1, null, null);
-        TreeNode root = new TreeNode(5, treeNode3, treeNode4);
-        return root;
-    }
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-
-        TreeNode(int x, TreeNode left, TreeNode right) {
-            this.val = x;
-            this.left = left;
-            this.right = right;
-
+    public void sortColors(int[] nums) {
+        int i = 0 ;
+        int j = nums.length-1;
+        int index = 0 ;
+        while (index <= j){
+            if (nums[index] == 0){
+                int temp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = temp;
+                i++ ;
+                index ++;
+            }else  if(nums[index] == 2){
+                int temp = nums[index];
+                nums[index] = nums[j];
+                nums[j] = temp;
+                j--;
+            }else {
+                index ++;
+            }
         }
     }
+
 
 }
