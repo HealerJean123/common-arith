f6e10cdb6a431c214783d76126dab0c39bc8180f
HealerJean <healerjean@gmail.com>
2020-12-03 18:45:45 +0800

    选择排序

--- src/main/java/com/hlj/arith/a_排序算法/排序算法之_3_直接插入排序.java
+++ src/main/java/com/hlj/arith/a_排序算法/排序算法之_3_直接插入排序.java
@@ -13,15 +13,14 @@ public class 排序算法之_3_直接插入排序 {
         System.out.println(Arrays.toString(nums));
     }
 
-
     public void 直接插入排序(int nums[]) {
         //从i等于1开始表示a[1] 也即是从第二个数字开始进行比较，进行n-1趟排序
         for (int i = 1; i < nums.length; i++) {
             // 待插入元素
             int temp = nums[i];
-            int j;
-            for (j = i; j > 0; j--) {
-                if (nums[j - 1] > temp) {
+            int j = i;
+            for (; j > 0; j--) {
+                if (temp < nums[j - 1] ) {
                     //执行完这个 j之后还要 继续执行下一个j-- ，j最后代表的就是 实际 带待插入元素的位置
                     nums[j] = nums[j - 1];
                 } else {
